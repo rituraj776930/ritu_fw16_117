d98c5718ed4cb6862ac33bb2e4a8f56629ca215a
rituraj776930 <102644137+RITU-RAJ-SINGH@users.noreply.github.com>
2022-05-07 19:24:41 +0530

    todo

---
+++ unit-4/assignments/src/sprint-3/day-4/assignments/components/SingleTodo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+function SingleTodo() {
+    const singledata = useSelector((state)=>state.singledata)
+  return (
+    <div>{singledata.title}</div>
+  )
+}
+
+export default SingleTodo
